180b43a21a2a255949c86874a78e1e93f4bceaaf
hef <hefan_482@163.com>
2023-04-14 22:54:43 +0800

    commit

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,7 +8,6 @@
 
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -23,6 +22,10 @@ impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
+
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
     // TODO: add another error conversion function here.
     // fn from_parseint...
 }
@@ -32,9 +35,13 @@ fn parse_pos_nonzero(s: &str)
 {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x)
-        .map_err(ParsePosNonzeroError::from_creation)
+    match s.parse::<i64>() {
+        Ok(val) => {
+            let x: i64 = val;
+            PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
+        }
+        Err(e) => Err(ParsePosNonzeroError::from_parseint(e)),
+    }
 }
 
 // Don't change anything below this line.
--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,11 +16,10 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -33,11 +32,17 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let lst = List::Cons(
+        2, 
+        Box::new(
+            List::Cons(
+                4, 
+                Box::new(List::Nil))));
+    lst
 }
 
 #[cfg(test)]
